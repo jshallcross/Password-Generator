65fa9e09991efe0f1138716d2a891838711d9ee1
Jon Shallcross <jshallcross1@icloud.com>
2020-12-19 11:27:35 -0500

    character arrays started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,18 @@
 // Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){
+// Charachter Arrays
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbolsArray = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","<",">","?",];
+
+// Empty Arrays
+var characterArray = [];
+var userPasswordArray = [];
+
 
 // Write password to the #password input
 function writePassword() {
