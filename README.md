19f73c953d06f75679354c5223be65ab91bc3b6b
Jon Shallcross <jshallcross1@icloud.com>
2020-12-20 13:02:17 -0500

    final touches/readme file completed

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Password Generator Starter Code
+## Function
+Once the user clicks the "Generate Password" button they are prompted for information.
+The user must select a pasword length from 8 - 128 characters.
+The user also must select which type of characters to allow in their password.
+Once the user has finished witht he prompts a new random password is displyed.
+
+## Languages
+HTML, CSS and JavaScript
+
+## Screenshot
+![SCreenshot of application](./assets/images/screenshot.png)
+
+## Author
+Jon Shallcross
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 var generateBtn = document.querySelector("#generate");
 //Generate Password Function
 function generatePassword(){
-// Charachter Arrays
+// Character Arrays
 var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -14,34 +14,34 @@ var characterArray = [];
 var userPasswordArray = [];
 
 // Prompt for user to choose length of their password
-var passwordLength = prompt("How long a password (Min 8 - Max 128?");
-  // loop to verify user has chosen a length of between 8 and 128 charachters
+var passwordLength = prompt("How long would you like your password to be? (Min 8 - Max 128)");
+  // loop to verify user has chosen a length of between 8 and 128 characters
   while (!passwordLength || parseInt(passwordLength) < 8 || parseInt(passwordLength) >= 129) {
-    var passwordLength = parseInt(prompt("How long a password (Min 8 - Max 128?"));
+    var passwordLength = parseInt(prompt("How long would you like your password to be? (Min 8 - Max 128)"));
   }
-  alert("Your password will be " + passwordLength + " chraracters long.");
+  alert("Thanks. Your password will be " + passwordLength + " chraracters long.");
 
-  var confirmLowerCase = (confirm("Click Okay to include lowercase charachters"));
+  var confirmLowerCase = (confirm("Click Okay to include lowercase characters in your password."));
   
-  var confirmUpperCase = (confirm("Click Okay to include UPPERCASE characters"));
+  var confirmUpperCase = (confirm("Click Okay to include UPPERCASE characters in your password."));
    
-  var confirmNumbers = (confirm("Click Okay to include numbers"));
+  var confirmNumbers = (confirm("Click Okay to include numbers(0-9) in your password."));
   
-  var confirmSymbols = (confirm("Click okay to add symbols to your password"));
+  var confirmSymbols = (confirm("Click okay to include special characters like($#&*) in your password."));
     // Loop to verify at least one character option was chosen
     while (confirmLowerCase === false && confirmUpperCase === false && confirmNumbers === false && confirmSymbols === false){
-    alert("You must select Okay for at least 1");
+    alert("Sorry, you must select Okay for at least 1 type of character.");
     // Asks again if the user does not select at least 1 character option
-    var confirmLowerCase = (confirm("Click Okay to include lowercase charachters"));
+    var confirmLowerCase = (confirm("Click Okay to include lowercase characters in your password."));
   
-    var confirmUpperCase = (confirm("Click Okay to include UPPERCASE characters"));
+    var confirmUpperCase = (confirm("Click Okay to include UPPERCASE characters in your password."));
   
-    var confirmNumbers = (confrim("Click Okay to include numbers"));
+    var confirmNumbers = (confirm("Click Okay to include numbers(0-9) in your password."));
   
-    var confirmSymbols = (confirm("Click okay to add symbols to your password"));
+    var confirmSymbols = (confirm("Click okay to include special characters like($#&*) in your password."));
   }
   
-//If statements to determine which charachter ooptions user has made
+//If statements to determine which character ooptions user has made
 //combines empty array with the character arrays based on user input
 if (confirmLowerCase){
   characterArray = characterArray.concat(lowercaseArray);
@@ -71,5 +71,26 @@ function writePassword() {
   passwordText.value = password;
 }
 
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');  // Create a <textarea> element
+  el.value = str;                                 // Set its value to the string that you want copied
+  el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
+  el.style.position = 'absolute';                 
+  el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+  document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+  const selected =            
+    document.getSelection().rangeCount > 0        // Check if there is any content selected previously
+      ? document.getSelection().getRangeAt(0)     // Store selection if found
+      : false;                                    // Mark as false to know no selection existed before
+  el.select();                                    // Select the <textarea> content
+  document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
+  document.body.removeChild(el);                  // Remove the <textarea> element
+  if (selected) {                                 // If a selection existed before copying
+    document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
+    document.getSelection().addRange(selected);   // Restore the original selection
+  }
+};
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
